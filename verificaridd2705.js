345c19fc9bc26fa0a31f3574249167b3e73e4818
jukiacosta <jukinhacosta09@gmail.com>
2025-05-27 17:08:09 -0300

    verificando idade vscode

---
+++ verificaridd2705.js
@@ -0,0 +1,11 @@
+// ler a idd do ususrio 
+
+// se idade for 18 ou mais, dizer que é maior 
+// senao, diz que é de menor 
+
+let idade = prompt("Qual sua idade??")
+if(idade>=18){
+    alert ("Você é de maior!")
+}else{
+    alert("Você é menor de idade!")
+}
